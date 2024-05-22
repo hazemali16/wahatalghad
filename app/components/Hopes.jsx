16bd5ea2f64773b604eaa3717909c522c1c07cbd
Hazem ali <81050076+hazemali16@users.noreply.github.com>
2024-05-22 19:17:29 +0300

    Update Hopes.jsx

--- app/components/Hopes.jsx
+++ app/components/Hopes.jsx
@@ -7,7 +7,7 @@ const Hopes = () => {
     <div className="w-10/12 mx-auto max-md:w-full flex items-center gap-5 max-lg:flex-col-reverse">
         <div className="w-1/2 max-lg:w-full">
             <h3 className="text-3xl text-[#37b8b8] mb-5 max-md:text-2xl">نهتم بالعمل على استراتيجية استقطاب الكفاءات</h3>
-            <p>التي تثمر بالثقة والمبادرات الإيجابية بعد خلق بيئة تحفز التفكير الإبداعي لدى المعلمين والمعلمات من أجل مصلحة أبناء وبنات مدارسة واحة الغد</p>
+            <p>التي تثمر بالثقة والمبادرات الإيجابية بعد خلق بيئة تحفز التفكير الإبداعي لدى المعلمين والمعلمات من أجل مصلحة أبناء وبنات مدارس واحة الغد</p>
         </div>
         <div className="w-1/2 max-lg:w-full">
         <Image src={hobes_image} alt="hobes_image" />
